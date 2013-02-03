4ca7ee050ae9fe8b005ff91a0eae434414ec9bab
Roy Barber <roy@roy-barber.co.uk>
2013-02-03 16:06:34 +0000

    initial commit

---
+++ config.rb
@@ -0,0 +1,15 @@
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "img"
+javascripts_dir = "js"
+
+# IMPORTANT - Use this if using css sprites
+relative_assets = true
+
+# You can select your preferred output style here (can be overridden via the command line):
+output_style = :compact
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+line_comments = false
